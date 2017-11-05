34bd41b26da7731833c7318e8b749f844a3fdd35
GalGog <galynna00@gmail.com>
2017-11-05 21:41:48 +0000

    <script> console </script>

---
+++ task.js
@@ -0,0 +1,15 @@
+alert('hello word');
+alert('first');
+alert('second');
+
+
+var message;
+message = 'hello hhfhj';
+alert (message);
+
+var newMessage = 'hi h i hi hi';
+alert(newMessage);
+
+var user = 'Sergey', age = 45, message = 'hi gogit';
+
+console.log('console log');
