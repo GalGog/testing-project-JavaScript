95dc162c62b67e98f3f52c265aa3aec586b62bec
GalGog <galynna00@gmail.com>
2017-11-07 14:22:37 +0000

    testing

--- task.js
+++ task.js
@@ -21,3 +21,23 @@ console.log(one+two);
 var vasyaDebt = 140;
 vasyaDebt = vasyaDebt -35;
 console.log (vasyaDebt);
+
+
+var COLOR_RED = "#F00";
+var color = COLOR_RED;
+alert(color);
+
+
+var userAge = 12;
+
+
+var userName = prompt('what is yous name');
+console.log (userName);
+
+/*prompt*/
+var userName = prompt('what is yous name','developer');
+console.log (userName);
+
+/*confirm*/
+var isAdullt = confirm('are you over 18?');
+console.log(isAdullt);
