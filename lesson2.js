a10c8ab8162c931d23908d85e1090a0501ac9308
GalGog <galynna00@gmail.com>
2017-11-12 15:24:46 +0000

    lesson2 hw task3-

--- lesson2.js
+++ lesson2.js
@@ -1,7 +1,47 @@
 var year = prompt('Какой сейчас год?', '');
-if (year == 2015) {
+if (year == 2017) {
     alert('Вы правы!');
 }
 else{
-    alert( 'С луны свалися? 2015!' );
+    alert( 'С луны свалися? 2017!' );
+}
+
+
+/* task2 ???*/
+var number = prompt('Введите любое целое число?', '');
+
+if (number == 1){
+    alert('1');
+}
+else if (number >0){
+    alert(' значение больше нуля ');
+}
+else if (number < 0 ){
+    alert('если значение меньше нуля, ');
+}
+
+
+
+/* task3 */
+var userName = prompt('ваш логин ?', '');
+
+if (userName == 'admin') {
+
+    var pass = prompt('Пароль?', '');
+
+    if (pass == 'passw0rd') {
+        alert( 'Welcome home!' );
+    } else if (pass == null) { // (*)
+        alert( 'Canceled' );
+    } else {
+        alert( 'Wrong password' );
+    }
+
+} else if (userName == null) { // (**)
+    alert( 'Canceled' );
+
+} else {
+
+    alert( 'Я вас не знаю' );
+
 }
