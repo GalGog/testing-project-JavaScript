8b3498ad7c11eca022995a9b9695693f5bbfec5f
GalGog <galynna00@gmail.com>
2017-11-30 22:54:40 +0000

    lesson4

---
+++ lessol4.3.js
@@ -0,0 +1,14 @@
+/*Создайте функцию truncate(str, maxlength), которая проверяет длину строки str, и если она превосходит maxlength – заменяет конец str на "...", так чтобы ее длина стала равна maxlength.
+
+Результатом функции должна быть (при необходимости) усечённая строка.*/
+function truncate(str, maxlength) {
+    if (str.length > maxlength) {
+        return str.slice(0, maxlength - 3) + '...';
+        // итоговая длина равна maxlength
+    }
+
+    return str;
+}
+
+alert( truncate("Вот, что мне хотелось бы сказать на эту тему:", 20) );
+alert( truncate("Всем привет!", 20) );
