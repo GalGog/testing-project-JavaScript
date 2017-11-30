b784329ccb5323d1278c17cf8c52d4c7ddf51677
GalGog <galynna00@gmail.com>
2017-11-30 22:52:31 +0000

    lesson4

--- lesson4.js
+++ lesson4.js
@@ -2,6 +2,10 @@
 checkSpam('get new Sex videos'); // true
 checkSpam('[SPAM] How to earn fast money?'); // true
 checkSpam('New PSD template'); // false
+
+Метод indexOf ищет совпадение с учетом регистра. То есть, в строке 'xXx' он не найдет 'XXX'.
+
+Для проверки, сначала приведем строку str к нижнему регистру, а затем уже будем искать.
 */
 function checkSpam(str) {
     var lowerStr = str.toLowerCase();
