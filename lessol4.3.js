38659843b57c33d108dfd66eb8414bac4597aa93
GalGog <galynna00@gmail.com>
2017-11-30 22:55:08 +0000

    lesson4

--- lessol4.3.js
+++ lessol4.3.js
@@ -12,3 +12,10 @@ function truncate(str, maxlength) {
 
 alert( truncate("Вот, что мне хотелось бы сказать на эту тему:", 20) );
 alert( truncate("Всем привет!", 20) );
+
+
+
+function truncate(str, maxlength) {
+    return (str.length > maxlength) ?
+        str.slice(0, maxlength - 3) + '...' : str;
+}
