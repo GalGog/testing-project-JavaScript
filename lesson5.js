e96dc9282e26afcbe3b942d4584718f1f7bd73ba
GalGog <galynna00@gmail.com>
2017-12-12 15:44:44 +0000

    lesson 5-5

--- lesson5.js
+++ lesson5.js
@@ -34,3 +34,18 @@ for (var code in codes) {
 
     alert( code + ": " + value ); // 7, 38, 1 во всех браузерах
 }
+
+
+
+var time = {
+    year: 2345,
+    month: 11,
+    day: 10,
+    hour: 11,
+    minute: 12,
+    second: 13,
+    microsecond: 123456
+}
+
+console.log(time); // (*)
+time.microsecond++; // (**)
