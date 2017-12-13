f20bbfb067b21cdaf239f352ffafc9eddb112fd8
GalGog <galynna00@gmail.com>
2017-12-13 13:56:21 +0000

    lesson5 push  Добавляет элемент в конец массива:

--- Массивы с числовыми индексами.js
+++ Массивы с числовыми индексами.js
@@ -10,3 +10,16 @@ alert( arr);
 var mas = [1, "name", {name: "Vova"},true];
 alert(mas[2].name);
 // получить объект из массива и тут же -- его свойство
+
+
+//push  Добавляет элемент в конец массива:
+
+var fruit = ["mango", "avokado", "gosga"]
+fruit.push ("bear");
+alert(fruit);
+
+var fruits = ["Яблоко", "Апельсин"];
+
+fruits.push("Груша");
+
+alert( fruits );
