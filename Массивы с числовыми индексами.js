9134502ba7a107ed81b39d41a44b67cc27e224c5
GalGog <galynna00@gmail.com>
2017-12-13 13:44:48 +0000

    lesson5 // получить объект из массива и тут же -- его свойство

--- Массивы с числовыми индексами.js
+++ Массивы с числовыми индексами.js
@@ -5,3 +5,8 @@ var arr = ["bus", "car", "cycle", "cycle"];
 arr[1] = "pedestrian";
 alert( arr.length);
 alert(arr[1]);
+alert( arr);
+
+var mas = [1, "name", {name: "Vova"},true];
+alert(mas[2].name);
+// получить объект из массива и тут же -- его свойство
