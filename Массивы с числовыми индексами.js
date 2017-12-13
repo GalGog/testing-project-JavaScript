c3ec0b5a6455d4b8dd90d0bc098269e7d56d5a89
GalGog <galynna00@gmail.com>
2017-12-13 13:36:13 +0000

    lesson5 Общее число элементов, хранимых в массиве, содержится в его свойстве length:

--- Массивы с числовыми индексами.js
+++ Массивы с числовыми индексами.js
@@ -1,5 +1,7 @@
 /**
  * Created by bakhtin on 13/12/2017.
  */
-var arr = ["bus", "car", "cycle"];
+var arr = ["bus", "car", "cycle", "cycle"];
+arr[1] = "pedestrian";
+alert( arr.length);
 alert(arr[1]);
