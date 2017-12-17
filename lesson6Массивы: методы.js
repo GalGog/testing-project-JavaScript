897cf89ce691c22563cf3fe10fcd87b95396d3b4
GalGog <galynna00@gmail.com>
2017-12-17 20:28:34 +0000

    Update lesson6Массивы: методы.js
    /*arr.splice(index[, deleteCount, elem1, ..., elemN])*/
    // удалить 3 первых элемента и добавить другие вместо них

--- lesson6Массивы: методы.js
+++ lesson6Массивы: методы.js
@@ -31,3 +31,15 @@ var arr = ["Я", "изучаю", "JavaScript"];
 arr.splice(1, 1); // начиная с позиции 1, удалить 1 элемент
 
 alert( arr ); //  осталось ["Я", "JavaScript"]
+
+
+
+
+
+/*arr.splice(index[, deleteCount, elem1, ..., elemN])*/
+var arr = ["Я", "сейчас", "изучаю", "JavaScript"];
+
+// удалить 3 первых элемента и добавить другие вместо них
+arr.splice(0, 3, "Мы", "изучаем")
+
+alert( arr ) // теперь ["Мы", "изучаем", "JavaScript"]
