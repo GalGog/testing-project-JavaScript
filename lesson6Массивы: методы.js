7b7bb3c82fccd8659e1ffeabed08ad0d79e8ee35
GalGog <galynna00@gmail.com>
2017-12-17 20:24:00 +0000

    Update lesson6Массивы: методы.js

--- lesson6Массивы: методы.js
+++ lesson6Массивы: методы.js
@@ -8,3 +8,9 @@ var arr = names.split(', ');
 for (var i = 0; i < arr.length; i++) {
   alert( 'Вам сообщение ' + arr[i] );
 }
+
+
+/*Вызов split с пустой строкой разобьёт по буквам:*/ 
+var str = "тест";
+
+alert( str.split('') ); // т,е,с,т
