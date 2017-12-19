2ac3d9b8894271f2387e0bea056f6b8ee22ef612
GalGog <galynna00@gmail.com>
2017-12-19 21:01:55 +0000

    Update lesson6Массив: перебирающие методы.js
    //Метод «arr.forEach(callback[, thisArg])» используется для перебора массива.

--- lesson6Массив: перебирающие методы.js
+++ lesson6Массив: перебирающие методы.js
@@ -1,3 +1,9 @@
 /**
  * Created by bakhtin on 16/12/2017.
  */
+//Метод «arr.forEach(callback[, thisArg])» используется для перебора массива.
+var arr = ["Яблоко", "Апельсин", "Груша"];
+
+arr.forEach(function(item, i, arr) {
+  alert( i + ": " + item + " (массив:" + arr + ")" );
+});
