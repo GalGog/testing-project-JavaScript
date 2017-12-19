e5f8ab9bc34b4f66049fa05d8e8df0471b72b577
GalGog <galynna00@gmail.com>
2017-12-19 21:24:40 +0000

    Update lesson6Массив: перебирающие методы.js

--- lesson6Массив: перебирающие методы.js
+++ lesson6Массив: перебирающие методы.js
@@ -9,7 +9,7 @@ arr.forEach(function(item, i, arr) {
 });
 
 //Метод «arr.filter(callback[, thisArg])» используется для фильтрации массива через функцию. Он создаёт новый массив, в который войдут только те элементы arr, для которых вызов callback(item, i, arr) возвратит true.
-var arr = [1, -1, 2, -2, 3];
+var arr = [1, -1, 2, -2, 13];
 
 var positiveArr = arr.filter(function(number) {
   return number > 0;
