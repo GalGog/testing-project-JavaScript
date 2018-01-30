5d6c58cf8d93f679d282d090bfb32014e3554aa5
GalGog <galynna00@gmail.com>
2018-01-30 21:11:59 +0000

    datetime date.getHours

---
+++ datetime.js
@@ -0,0 +1,9 @@
+// текущая дата
+var date = new Date();
+
+// час в текущей временной зоне
+alert( date.getHours() );
+
+// сколько сейчас времени в Лондоне?
+// час в зоне GMT+0
+alert( date.getUTCHours() );
