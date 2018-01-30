5f26deb8d19fc822b94191ddbb75b943f170fdd1
GalGog <galynna00@gmail.com>
2018-01-30 21:27:04 +0000

    datetime date.getHours

--- datetime.js
+++ datetime.js
@@ -34,3 +34,26 @@ for (var i = 0; i < 100000; i++) {
 var end = new Date; // конец измерения
 
 alert( "Цикл занял " + (end - start) + " ms" );
+
+
+
+
+var arr = [];
+for (var i = 0; i < 1000; i++) arr[i] = 0;
+
+function walkIn(arr) {
+    for (var key in arr) arr[key]++;
+}
+
+function walkLength(arr) {
+    for (var i = 0; i < arr.length; i++) arr[i]++;
+}
+
+function bench(f) {
+    var date = new Date();
+    for (var i = 0; i < 10000; i++) f(arr);
+    return new Date() - date;
+}
+
+alert( 'Время walkIn: ' + bench(walkIn) + 'мс' );
+alert( 'Время walkLength: ' + bench(walkLength) + 'мс' );
