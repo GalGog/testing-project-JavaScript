d46917a3ed1296d5389ddde9a7931836b6a01548
GalGog <galynna00@gmail.com>
2018-01-31 15:28:03 +0000

    datetime date.getHours - Метод getDay() позволяет получить номер дня недели, начиная с воскресенья.
    - В современных браузерах можно использовать и toLocaleString:

--- datetime.js
+++ datetime.js
@@ -65,3 +65,27 @@ alert( 'Время walkLength: ' + bench(walkLength) + 'мс' );
 
     var d = new Date(2012, 1, 20, 3, 12);
 alert( d );
+
+
+
+
+//task2
+//Создайте функцию getWeekDay(date), которая выводит текущий день недели в коротком формате „пн“, „вт“, … „вс“.
+
+
+//Метод getDay() позволяет получить номер дня недели, начиная с воскресенья.
+
+    function getWeekDay(date) {
+        var days = ['вс', 'пн', 'вт', 'ср', 'чт', 'пт', 'сб'];
+
+        return days[date.getDay()];
+    }
+
+var date = new Date(2014, 0, 3); // 3 января 2014
+alert( getWeekDay(date) ); // 'пт'
+
+
+//!!В современных браузерах можно использовать и toLocaleString:
+//!!!!!!
+    var date = new Date(2014, 0, 3); // 3 января 2014
+alert( date.toLocaleString('ru', {weekday: 'short'}) ); // 'Пт'
