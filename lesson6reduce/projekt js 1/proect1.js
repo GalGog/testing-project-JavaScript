19d6e488a4e77a5678192a2c0d060a088eb2b054
GalGog <galynna00@gmail.com>
2018-02-06 10:55:08 +0000

    practic lesson

---
+++ lesson6reduce/projekt js 1/proect1.js
@@ -0,0 +1,11 @@
+var boxer = new Object();
+
+boxer.weight = 70;//kg
+boxer.height = 180;//cm
+boxer.name = "Ivan";
+alert(boxer.height +  'cm');
+
+boxer.family = 'abramenco';
+boxer.razryad = 2;
+
+alert (boxer.height + 'cm');
