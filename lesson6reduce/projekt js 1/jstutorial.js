30b3527467ca22b36911541896175fb01bc4a2c0
GalGog <galynna00@gmail.com>
2018-02-06 15:31:38 +0000

    practic lesson

--- lesson6reduce/projekt js 1/jstutorial.js
+++ lesson6reduce/projekt js 1/jstutorial.js
@@ -8,3 +8,14 @@ if ((age >=5) && (age<=6)){
 }
 
 document.write ("true || false", true || false,  + "br/");
+
+
+
+
+
+
+document.write("Converted String :",Number("3.14"), "<br/>");
+
+var randStr = "A long" + "string that" + "goes on and on";
+document.write("String Length", randStr.length + "<br/>");
+document.write ("index for \"goes\": ",randStr.indexOf("goes"), "<br/>");
