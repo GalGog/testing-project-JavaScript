61a28034633e98a7c4692b0bd8b336fe87c0bd38
GalGog <galynna00@gmail.com>
2018-02-06 11:03:27 +0000

    practic lesson

--- lesson6reduce/projekt js 1/proect1.js
+++ lesson6reduce/projekt js 1/proect1.js
@@ -3,9 +3,11 @@ var boxer = new Object();
 boxer.weight = 70;//kg
 boxer.height = 180;//cm
 boxer.name = "Ivan";
-alert(boxer.height +  'cm');
-
 boxer.family = 'abramenco';
 boxer.razryad = 2;
 
-alert (boxer.height + 'cm');
+boxer.sayWeight = function (n) {
+    alert(n + 'kg');
+
+}
+boxer.sayWeight(boxer.weight);
