c3a259b075bb0e01d986d999c8d1dadbb7f06837
GalGog <galynna00@gmail.com>
2018-02-06 12:32:24 +0000

    practic lesson

---
+++ lesson6reduce/projekt js 1/jstutorial.js
@@ -0,0 +1,10 @@
+var age = 5;
+if ((age >=5) && (age<=6)){
+    document.write ("Go to  Kingarden" + "<br/");
+} else if (age >18) {
+    document.write ("Go to  college" + "<br/");
+} else {
+    document.write ("Go to  grade", age - 5, + "<br/");
+}
+
+document.write ("true || false", true || false,  + "br/");
