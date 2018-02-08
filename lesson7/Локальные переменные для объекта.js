ce527789cacfaa0b26a72afde018422c3ea4f943
GalGog <galynna00@gmail.com>
2018-02-08 15:51:21 +0000

    lesson7

---
+++ lesson7/Локальные переменные для объекта.js
@@ -0,0 +1,25 @@
+function makeCounter() {
+    var currentCount = 1;
+
+    return { // возвратим объект вместо функции
+        getNext: function() {
+            return currentCount++;
+        },
+
+        set: function(value) {
+            currentCount = value;
+        },
+
+        reset: function() {
+            currentCount = 1;
+        }
+    };
+}
+
+var counter = makeCounter();
+
+alert( counter.getNext() ); // 1
+alert( counter.getNext() ); // 2
+
+counter.set(5);
+alert( counter.getNext() ); // 5
