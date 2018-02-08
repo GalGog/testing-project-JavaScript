aadd8dcfffdce420da38e0d964dbd684ecdb4a9d
GalGog <galynna00@gmail.com>
2018-02-08 16:11:41 +0000

    lesson7

--- lesson7/Локальные переменные для объекта.js
+++ lesson7/Локальные переменные для объекта.js
@@ -101,3 +101,35 @@ buffer2(1);
 buffer2(0);
 
 alert( buffer2() ); // '010'
+
+
+//
+var users = [{
+    name: "Вася",
+    surname: 'Иванов',
+    age: 20
+}, {
+    name: "Петя",
+    surname: 'Чапаев',
+    age: 25
+}, {
+    name: "Маша",
+    surname: 'Медведева',
+    age: 18
+}];
+
+function byField(field) {
+    return function(a, b) {
+        return a[field] > b[field] ? 1 : -1;
+    }
+}
+
+users.sort(byField('name'));
+users.forEach(function(user) {
+    alert( user.name );
+});
+
+users.sort(byField('age'));
+users.forEach(function(user) {
+    alert( user.name );
+});
