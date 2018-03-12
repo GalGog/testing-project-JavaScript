c9bbe4374d2699cdb6ee15cf3ee40457e12905d6
GalGog <galynna00@gmail.com>
2018-03-12 16:53:33 +0000

    testing js Методы объектов, this

--- lesson8/lesson8.1.js
+++ lesson8/lesson8.1.js
@@ -118,3 +118,22 @@ var calculator = {
 calculator.read();
 alert( calculator.sum() );
 alert( calculator.mul() );
+
+//чейнинг» (chaining)
+var ladder = {
+    step: 0,
+    up: function() {
+        this.step++;
+        return this;
+    },
+    down: function() {
+        this.step--;
+        return this;
+    },
+    showStep: function() {
+        alert( this.step );
+        return this;
+    }
+}
+
+ladder.up().up().down().up().down().showStep();
