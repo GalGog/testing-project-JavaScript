346fc19656f6ff33d5258d2fe76e42535629f8ca
GalGog <galynna00@gmail.com>
2018-03-12 16:13:00 +0000

    testing js Методы объектов, this

---
+++ lesson8/lesson8.1.js
@@ -0,0 +1,38 @@
+//Методы у объектов
+var user = {
+    name: 'Василий',
+
+    // метод
+    sayHi: function() {
+        alert( 'Привет!' );
+    }
+
+};
+
+// Вызов
+user.sayHi();
+
+var user = {
+    name: 'Василий',
+
+    sayHi: function() {
+        alert( this.name );
+    }
+};
+
+user.sayHi(); // sayHi в контексте user
+
+
+var user = {
+    name: 'Василий',
+
+    sayHi: function() {
+        showName(this); // передать текущий объект в showName
+    }
+};
+
+function showName(namedObj) {
+    alert( namedObj.name );
+}
+
+user.sayHi(); // Василий
